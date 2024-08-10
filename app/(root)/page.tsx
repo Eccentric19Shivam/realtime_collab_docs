aee9cdcf464f223043740bff27b7f6f91a06897f
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-08-10 16:20:27 +0530

    before auth: Wish me luck

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Button>CLick ME</Button>
+    </div>
+  )
+}
+
+export default Home

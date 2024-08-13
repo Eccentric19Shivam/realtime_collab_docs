76ef8f7e1bd1db627b4e18c0e8d836559ea667f5
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-08-13 23:30:49 +0530

    implemented sentry, delete and share modals and notifications Done finally

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input"
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import ShareModal from "./ShareModal";
 
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
@@ -106,6 +107,14 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
 
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveColllaborators />
+
+              <ShareModal 
+                roomId={roomId}
+                collaborators={users}
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              />
+
               <SignedOut>
                 <SignInButton />
               </SignedOut>
